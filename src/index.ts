62b10b31b313375bac27754b27efd3768ef7ae29
Hibiki Sakuraoka <wbydo@users.noreply.github.com>
2023-04-14 00:31:57 +0900

    string, number

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,53 @@
-console.log('Hello World!!');
+import {
+  factory,
+  NodeFlags,
+  createPrinter,
+  createSourceFile,
+  VariableStatement,
+  ScriptTarget,
+  ScriptKind,
+  EmitHint,
+} from 'typescript';
+
+export const parse = (prefix: string, input: unknown): VariableStatement => {
+  if (typeof input === 'string' || typeof input === 'number') {
+    return factory.createVariableStatement(
+      undefined,
+      factory.createVariableDeclarationList(
+        [
+          factory.createVariableDeclaration(
+            factory.createIdentifier(`${prefix}Schema`),
+            undefined,
+            undefined,
+            factory.createCallExpression(
+              factory.createPropertyAccessExpression(
+                factory.createIdentifier('z'),
+                factory.createIdentifier(typeof input)
+              ),
+              undefined,
+              []
+            )
+          ),
+        ],
+        NodeFlags.Const
+      )
+    );
+  } else {
+    throw new Error();
+  }
+};
+
+export const toString = (v: VariableStatement): string => {
+  // https://github.com/microsoft/TypeScript-wiki/blob/main/Using-the-Compiler-API.md#creating-and-printing-a-typescript-ast
+
+  const resultFile = createSourceFile(
+    '', // ファイル名(今回は使わないので空文字)
+    '',
+    ScriptTarget.Latest,
+    /*setParentNodes*/ false,
+    ScriptKind.TS
+  );
+  const printer = createPrinter();
+
+  return printer.printNode(EmitHint.Unspecified, v, resultFile);
+};
--- tests/hoge.test.ts
+++ tests/hoge.test.ts
@@ -1,6 +1,85 @@
-describe.skip('greet', (): void => {
-  test('should say hello to Tom.', (): void => {
-    const response: string = 'Hello, Tom!';
-    expect(response).toBe('Hello, Tom!');
+import { factory, NodeFlags } from 'typescript';
+
+import { parse, toString } from '../src';
+
+describe('string', (): void => {
+  const prefix = 'prefix';
+  const input = 'foo';
+  const ast = parse(prefix, input);
+
+  test('AST', (): void => {
+    const actual = ast;
+
+    const expected = factory.createVariableStatement(
+      undefined,
+      factory.createVariableDeclarationList(
+        [
+          factory.createVariableDeclaration(
+            factory.createIdentifier(`${prefix}Schema`),
+            undefined,
+            undefined,
+            factory.createCallExpression(
+              factory.createPropertyAccessExpression(
+                factory.createIdentifier('z'),
+                factory.createIdentifier('string')
+              ),
+              undefined,
+              []
+            )
+          ),
+        ],
+        NodeFlags.Const
+      )
+    );
+
+    expect(actual).toStrictEqual(expected);
+  });
+
+  test('code', (): void => {
+    const actual = toString(ast);
+    const expected = `const ${prefix}Schema = z.string();`;
+
+    expect(actual).toBe(expected);
+  });
+});
+
+describe('number', (): void => {
+  const prefix = 'prefix';
+  const input = 1234;
+  const ast = parse(prefix, input);
+
+  test('AST', (): void => {
+    const actual = ast;
+
+    const expected = factory.createVariableStatement(
+      undefined,
+      factory.createVariableDeclarationList(
+        [
+          factory.createVariableDeclaration(
+            factory.createIdentifier(`${prefix}Schema`),
+            undefined,
+            undefined,
+            factory.createCallExpression(
+              factory.createPropertyAccessExpression(
+                factory.createIdentifier('z'),
+                factory.createIdentifier('number')
+              ),
+              undefined,
+              []
+            )
+          ),
+        ],
+        NodeFlags.Const
+      )
+    );
+
+    expect(actual).toStrictEqual(expected);
+  });
+
+  test('code', (): void => {
+    const actual = toString(ast);
+    const expected = `const ${prefix}Schema = z.number();`;
+
+    expect(actual).toBe(expected);
   });
 });
